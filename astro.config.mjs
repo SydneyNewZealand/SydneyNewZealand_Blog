ed288fd6e304156fedbec90e1ce89a7592dcb5a8
雪梨纽西兰希思露甘奶迪 <92838519+SydneyNewZealand@users.noreply.github.com>
2025-09-05 08:59:39 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 // https://astro.build/config
 export default defineConfig({
 	site: "https://SydneyNewZealand.github.io",
-	base: "SydneyNewZealand_Blog",
+	base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
